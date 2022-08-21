210fdc4fec914e76b4fac2b3e3ddf1aa42a45f92
vangohao <vangohao@gmail.com>
2022-08-21 19:55:45 +0800

    issue and pr

--- api/api.py
+++ api/api.py
@@ -4,7 +4,7 @@ import requests
 
 app = Flask(__name__)
 
-f = open("url");
+f = open("url")
 url = f.read()
 f.close()
 
@@ -22,6 +22,21 @@ def send(data):
     response = requests.post(url, json=result)
     print("response code: ", response.status_code)
 
+def parse_labels(labels):
+    labels_list = []
+    colors_list = []
+    for label in labels:
+        labels_list.append(label["name"])
+        colors_list.append(label["color"])
+    
+    label_string = ""
+    for label in labels_list:
+        label_string = label_string + "**" + label + "** "
+    return label_string
+
+def remove_cr(text):
+    return "\n".join(text.splitlines())
+
 @app.route('/')
 def hello():
     return 'Webhooks with Python'
@@ -30,6 +45,8 @@ def hello():
 def github():
     data = request.json
     event = request.headers["X-Github-Event"]
+
+    flag = True
     if event == "ping":
         return "ping"
     elif event == "push":
@@ -46,11 +63,66 @@ def github():
         result["elements"][1]["fields"][0]["text"]["content"] = "**head commit id**\n" + head_commit_id
         result["elements"][1]["fields"][1]["text"]["content"] = "**head commit author**\n" + head_commit_author_name
         result["header"]["title"]["content"] = "@" + pusher + " pushed to " + repo_name + "/" + branch
-    # print(f'Issue {data["issue"]["title"]} {data["action"]}')
-    # print(f'{data["issue"]["body"]}')
-    # print(f'{data["issue"]["url"]}')
+    elif event == "issues":
+        event_type = "issue"
+        event_action = data["action"]
+
+        event_created_at = data[event_type]["created_at"]
+        event_updated_at = data[event_type]["updated_at"]
+        event_url = data[event_type]["url"]
+        repo_name = data["repository"]["name"]
+        event_number = data[event_type]["number"]
+        event_title = data[event_type]["title"]
+        event_user = data[event_type]["user"]["login"]
+        event_labels = data[event_type]["labels"]
+        event_body = data[event_type]["body"]
+
+        result = get_template(event_type)
+        result["elements"][0]["text"]["content"] = "**Title: **[" + event_title + "]("+event_url+")"
+        result["elements"][1]["text"]["content"] = "labels: " + parse_labels(event_labels)
+        result["elements"][2]["text"]["content"] = remove_cr(event_body)
+        result["header"]["title"]["content"]= "@" + event_user + " " + event_action + " issue #" + str(event_number) + " in " + repo_name
+
+        
+        if (event_created_at == event_updated_at) and event_action != "opened":
+            flag = False
+
+    elif event == "pull_request":
+        event_type = "pull_request"
+        event_action = data["action"]
+
+        event_created_at = data[event_type]["created_at"]
+        event_updated_at = data[event_type]["updated_at"]
+        event_url = data[event_type]["url"]
+        repo_name = data["repository"]["name"]
+        event_number = data[event_type]["number"]
+        event_title = data[event_type]["title"]
+        event_user = data[event_type]["user"]["login"]
+        event_labels = data[event_type]["labels"]
+        event_body = data[event_type]["body"]
+
+        head_branch = data[event_type]["head"]["label"]
+        base_branch = data[event_type]["base"]["label"]
+
+
+        result = get_template(event_type)
+        result["elements"][0]["text"]["content"] = "**Title: **[" + event_title + "]("+event_url+")"
+        result["elements"][1]["text"]["content"] = "labels: " + parse_labels(event_labels)
+        result["elements"][2]["text"]["content"] = remove_cr(event_body)
+
+        result["elements"][3]["fields"][0]["text"]["content"] = "**From**\n" + head_branch
+        result["elements"][3]["fields"][1]["text"]["content"] = "**To**\n" + base_branch
+
+        result["header"]["title"]["content"]= "@" + event_user + " " + event_action + " pull request #" + str(event_number) + " in " + repo_name
+
+        
+        if (event_created_at == event_updated_at) and event_action != "opened":
+            flag = False
+    
+
     print( json.dumps(result))
-    send(result)
+    if flag:
+        send(result)
     return json.dumps(result)
 
 
